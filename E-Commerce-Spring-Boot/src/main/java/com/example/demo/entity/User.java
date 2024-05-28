dcc4fc5ca224f2b3a38518de7cd6ae0e591909bc
wiemtouj <toujwiem@gmail.com>
2024-05-28 15:17:44 +0100

    update

--- E-Commerce-Spring-Boot/src/main/java/com/example/demo/entity/User.java
+++ E-Commerce-Spring-Boot/src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,80 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "users")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	@Column(unique = true)
+	private String username;
+	
+	private String email;
+	
+	private String password;
+	
+	private UserRole role;
+	
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+		
 
 }
