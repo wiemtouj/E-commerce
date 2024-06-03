8d7350243fa38c149690cef9529e9dc3ef47b075
wiemtouj <toujwiem@gmail.com>
2024-06-03 23:29:59 +0100

    Create new  entity product.java

---
+++ E-Commerce-Spring-Boot/src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,43 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Product {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private String description;
+    // Getters and setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+    
+    
+
+}
