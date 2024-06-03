a4d0e883eea66b8ddcfeb84d7c2dbae262d65e3e
wiemtouj <toujwiem@gmail.com>
2024-06-04 00:05:29 +0100

    Add new package controller

---
+++ E-Commerce-Spring-Boot/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+	
+	@Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+}
